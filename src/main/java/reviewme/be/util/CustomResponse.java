acf5ed5584425c6e810fd7b22a023c2bc7c540e0
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-11-15 12:47:08 +0900

    feat: 원하는 형식의 데이터를 반환할 수 있도록 Generic 형식 Response 생성(#2) (#3)

---
+++ src/main/java/reviewme/be/util/CustomResponse.java
@@ -0,0 +1,20 @@
+package reviewme.be.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomResponse<T> {
+
+    private String status;
+    private int code;
+    private String message;
+    private T data;
+
+    public CustomResponse(String status, int code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
