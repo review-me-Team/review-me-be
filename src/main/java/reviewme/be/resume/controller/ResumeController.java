50f0bfa532387e3572daa141f756df0d7660240c
luke <gseonggyu968@gmail.com>
2023-11-23 08:41:48 +0900

    feat: 이력서 업로드 API 문서 작성(#8)

--- src/main/java/reviewme/be/resume/controller/ResumeController.java
+++ src/main/java/reviewme/be/resume/controller/ResumeController.java
@@ -1,16 +1,20 @@
 package reviewme.be.resume.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import reviewme.be.resume.request.UploadResumeRequest;
 import reviewme.be.resume.response.ResumePageResponse;
 import reviewme.be.resume.response.ResumeResponse;
+import reviewme.be.resume.response.UploadResumeResponse;
 import reviewme.be.util.CustomResponse;
 
 import java.time.LocalDateTime;
@@ -48,6 +52,32 @@ public class ResumeController {
                         ResumePageResponse.builder()
                                 .resumePage(sampleResponse)
                                 .build()
+                ));
+    }
+
+    @Operation(summary = "resume", description = "이력서를 업로드합니다.")
+    @PostMapping
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "이력서 업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "이력서 업로드 실패")
+    })
+    public ResponseEntity<CustomResponse<UploadResumeResponse>> uploadResume(
+            @Parameter(description = "PDF 파일", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "file", format = "binary")))
+            @RequestPart MultipartFile pdf,
+            @RequestPart UploadResumeRequest uploadResumeRequest
+    ) {
+
+        UploadResumeResponse createdResumeId = UploadResumeResponse.builder()
+                .id(1L)
+                .build();
+
+        return ResponseEntity
+                .ok()
+                .body(new CustomResponse<>(
+                        "success",
+                        200,
+                        "이력서 업로드에 성공했습니다.",
+                        createdResumeId
                 ));
     }
 }
---
+++ src/main/java/reviewme/be/resume/response/UploadResumeResponse.java
@@ -0,0 +1,14 @@
+package reviewme.be.resume.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "이력서 업로드 응답")
+public class UploadResumeResponse {
+
+    @Schema(description = "이력서 ID", example = "1")
+    private long id;
+}
