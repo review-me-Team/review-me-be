00c0467158142a52e91bb666178c0339b4dbd61b
luke <gseonggyu968@gmail.com>
2023-12-23 00:37:16 +0900

    feat: 피드백 엔티티 생성(#10)

---
+++ src/main/java/reviewme/be/feedback/entity/Feedback.java
@@ -0,0 +1,43 @@
+package reviewme.be.feedback.entity;
+
+import lombok.*;
+import reviewme.be.resume.entity.Resume;
+import reviewme.be.util.entity.Label;
+import reviewme.be.util.entity.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User writer;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Feedback parentFeedback;
+
+    @OneToOne
+    @JoinColumn(name = "label_id")
+    private Label label;
+
+    @OneToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
+
+    private int resumePage;
+    private Boolean checked;
+    private int childCnt;
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
+}
