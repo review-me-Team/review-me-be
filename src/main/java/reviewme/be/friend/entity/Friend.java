5bf534e1da3c16f81689c9c06e86b8dc654cd86b
luke <gseonggyu968@gmail.com>
2023-12-22 23:27:31 +0900

    feat: 친구 엔티티 생성(#10)

---
+++ src/main/java/reviewme/be/friend/entity/Friend.java
@@ -0,0 +1,28 @@
+package reviewme.be.friend.entity;
+
+import lombok.*;
+import reviewme.be.util.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "follower_id")
+    private User followerUser;
+
+    @OneToOne
+    @JoinColumn(name = "following_id")
+    private User followingUser;
+
+    private Boolean accepted;
+}
