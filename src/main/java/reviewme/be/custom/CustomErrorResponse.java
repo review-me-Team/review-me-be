0b7e9db8cfd4a10ad45e7efdc90e6c391cc98c15
luke <gseonggyu968@gmail.com>
2023-12-29 01:08:02 +0900

    feat: API 문서를 명확하게 하기 위해 스웨거 애노테이션 추가(#16)

--- src/main/java/reviewme/be/custom/CustomErrorResponse.java
+++ src/main/java/reviewme/be/custom/CustomErrorResponse.java
@@ -1,5 +1,6 @@
 package reviewme.be.custom;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,9 +8,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CustomErrorResponse {
 
+    @Schema(description = "성공 여부", example = "fail")
     private String status;
+
+    @Schema(description = "응답 코드", example = "400")
     private int code;
+
+    @Schema(description = "응답 메시지", example = "요청 실패 원인 등")
     private String message;
+
+    @Schema(description = "응답 데이터")
     private Object data = null;
 
     public CustomErrorResponse(String status, int code, String message) {
