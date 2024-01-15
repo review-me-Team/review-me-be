416d785593ccb2f1d30bfb8eac372c2311a742d7
luke <gseonggyu968@gmail.com>
2024-01-15 18:29:38 +0900

    feat: 존재하지 않는 user에게 친구 요청 시 예외 처리(#36)

--- src/main/java/reviewme/be/user/controller/UserExceptionHandler.java
+++ src/main/java/reviewme/be/user/controller/UserExceptionHandler.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reviewme.be.custom.CustomErrorResponse;
 import reviewme.be.user.exception.InvalidCodeException;
+import reviewme.be.user.exception.NonExistUserException;
 
 @RestControllerAdvice
 public class UserExceptionHandler {
 
     @ExceptionHandler(InvalidCodeException.class)
     public ResponseEntity<CustomErrorResponse> invalidCode(InvalidCodeException ex) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(new CustomErrorResponse(
+                        "Bad Request",
+                        400,
+                        ex.getMessage()));
+    }
+
+    @ExceptionHandler(NonExistUserException.class)
+    public ResponseEntity<CustomErrorResponse> nonExistUser(NonExistUserException ex) {
 
         return ResponseEntity
                 .badRequest()
