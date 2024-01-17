b1383a5525af81be8b53644ec10f8ddc37001aa3
luke <gseonggyu968@gmail.com>
2024-01-18 00:21:27 +0900

    chore: IP를 gitignore에 추가되도록 함(#40)

--- src/main/java/reviewme/be/config/CorsConfig.java
+++ src/main/java/reviewme/be/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package reviewme.be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${SERVER_URL}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://13.209.230.48")
+                .allowedOrigins("allowedOrigins")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
                 .allowCredentials(true)
                 .allowedHeaders("*");
