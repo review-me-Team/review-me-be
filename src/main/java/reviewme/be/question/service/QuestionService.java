e979399cf915424684f7e09952ec93c39544493d
luke <gseonggyu968@gmail.com>
2024-02-08 16:42:59 +0900

    feat: 유효한 이력서인지 검증 추가(#67)

--- src/main/java/reviewme/be/question/service/QuestionService.java
+++ src/main/java/reviewme/be/question/service/QuestionService.java
@@ -3,6 +3,7 @@ package reviewme.be.question.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reviewme.be.question.repository.QuestionRepository;
+import reviewme.be.resume.service.ResumeService;
 import reviewme.be.util.entity.Label;
 import reviewme.be.util.repository.LabelRepository;
 
@@ -13,9 +14,13 @@ import java.util.List;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final ResumeService resumeService;
     private final LabelRepository labelRepository;
 
     public List<Label> findQuestionLabels(long resumeId) {
+
+        // 이력서 존재 여부 확인
+        resumeService.findById(resumeId);
 
         return labelRepository.findByResumeId(resumeId);
     }
