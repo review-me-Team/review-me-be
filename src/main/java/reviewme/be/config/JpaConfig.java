e1063e18abb4a6c71fd2184e444d9e5c542e3889
luke <gseonggyu968@gmail.com>
2024-03-15 16:58:52 +0900

    feat: JPA config 생성(#94)

---
+++ src/main/java/reviewme/be/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package reviewme.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
