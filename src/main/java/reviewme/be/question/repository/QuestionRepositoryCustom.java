e72008c670f3994ebeca331339ce24b72e35570d
luke <gseonggyu968@gmail.com>
2024-03-28 17:04:27 +0900

    feat: 체크, 북마크 여부에 따라 예상 질문 목록 조회 기능 수정(#122)

--- src/main/java/reviewme/be/question/repository/QuestionRepositoryCustom.java
+++ src/main/java/reviewme/be/question/repository/QuestionRepositoryCustom.java
@@ -3,13 +3,14 @@ package reviewme.be.question.repository;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import reviewme.be.question.dto.QuestionsFilter;
 import reviewme.be.question.dto.response.QuestionCommentResponse;
 import reviewme.be.question.dto.response.QuestionResponse;
 import reviewme.be.question.entity.Question;
 
 public interface QuestionRepositoryCustom {
 
-    Page<QuestionResponse> findQuestionsByResumeIdAndResumePage(long resumeId, long userId, int resumePage, Pageable pageable);
+    Page<QuestionResponse> findQuestionsByResumeIdAndResumePage(long resumeId, long userId, QuestionsFilter filter, Pageable pageable);
 
     Page<QuestionCommentResponse> findQuestionCommentsByQuestionId(long questionId, long userId, Pageable pageable);
 
